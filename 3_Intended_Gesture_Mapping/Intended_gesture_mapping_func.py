6d70b5671403829b019682b784aa1883db503076
ChamikaRohan <chamika707@gmail.com>
2024-04-11 13:49:47 +0530

    Created intended-gesture-map functionality

---
+++ 3_Intended_Gesture_Mapping/Intended_gesture_mapping_func.py
@@ -0,0 +1,30 @@
+#consecutive_frames = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0}
+def intended_gesture_map(predicted_class, consecutive_frames):
+    if predicted_class == 0:
+        consecutive_frames[0] += 1
+    else:
+        consecutive_frames[0] = 0
+
+    if predicted_class == 1:
+        consecutive_frames[1] += 1
+    else:
+        consecutive_frames[1] = 0
+
+    if predicted_class == 2:
+        consecutive_frames[2] += 1
+    else:
+        consecutive_frames[2] = 0
+
+    if predicted_class == 3:
+        consecutive_frames[3] += 1
+    else:
+        consecutive_frames[3] = 0
+
+    if predicted_class == 4:
+        consecutive_frames[4] += 1
+    else:
+        consecutive_frames[4] = 0
+
+    for gesture, count in consecutive_frames.items():
+        if count == 5:
+            return gesture
---
+++ 3_Intended_Gesture_Mapping/Source_v1_using_time.py
@@ -0,0 +1,114 @@
+import cv2
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+import mediapipe as mp
+import time
+
+def predict_gesture(cap, model_path):
+    # Load the trained model
+    model = load_model(model_path)
+
+    _, first_frame = cap.read()
+    first_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
+    first_gray = cv2.GaussianBlur(first_gray, (5, 5), 0)
+
+    # Initialize MediaPipe Hands
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5)
+
+    start_time = time.time()
+    gesture_count = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0}
+
+    while True:
+        _, frame = cap.read()
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
+
+        difference = cv2.absdiff(first_gray, gray_frame)
+        _, difference = cv2.threshold(difference, 25, 255, cv2.THRESH_BINARY)
+
+        # Find hand landmarks using MediaPipe
+        results = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                # Extract hand region coordinates
+                x_min, y_min = frame.shape[1], frame.shape[0]
+                x_max, y_max = 0, 0
+                for landmark in hand_landmarks.landmark:
+                    x, y = int(landmark.x * frame.shape[1]), int(landmark.y * frame.shape[0])
+                    if x < x_min:
+                        x_min = x
+                    if x > x_max:
+                        x_max = x
+                    if y < y_min:
+                        y_min = y
+                    if y > y_max:
+                        y_max = y
+
+                # Crop hand region from the difference frame with some padding
+                padding = 60
+                hand_crop = difference[max(0, y_min - padding):min(y_max + padding, difference.shape[0]),
+                            max(0, x_min - padding):min(x_max + padding, difference.shape[1])]
+
+                resized_img = cv2.resize(hand_crop, (64, 64))
+
+                # Convert grayscale to RGB by repeating the single channel
+                img_rgb = cv2.cvtColor(resized_img, cv2.COLOR_GRAY2RGB)
+
+                # Expand dimensions to create a batch of 1 image
+                img_array = np.expand_dims(img_rgb, axis=0)
+                img_array = img_array.astype('float32') / 255.0  # Normalize to [0, 1]
+
+                # Make predictions
+                predictions = model.predict(img_array)
+
+                # Get the predicted class label
+                predicted_class = np.argmax(predictions)
+
+                # Get the probability of the predicted class
+                predicted_probability = predictions[0][predicted_class]
+
+                if predicted_class == 0:
+                    if predicted_probability > 0.9999:
+                        gesture_count[0] += 1
+
+                elif predicted_class == 1:
+                    if predicted_probability > 0.99:
+                        gesture_count[1] += 1
+
+                elif predicted_class == 2:
+                    if predicted_probability > 0.85:
+                        gesture_count[2] += 1
+
+                elif predicted_class == 3:
+                    if predicted_probability > 0.9:
+                        gesture_count[3] += 1
+
+                elif predicted_class == 4:
+                    if predicted_probability > 0.87:
+                        gesture_count[4] += 1
+
+        cv2.imshow("First frame", first_frame)
+        cv2.imshow("Frame", frame)
+        cv2.imshow("Difference", difference)
+
+        # Check if 1 seconds have passed
+        if time.time() - start_time >=1:
+            max_gesture = max(gesture_count, key=gesture_count.get)
+            if gesture_count[max_gesture] > 0:
+                print("Gesture in the last 1 seconds:", max_gesture)
+            else:
+                print("No gesture detected in the last 1 seconds.")
+            start_time = time.time()
+            gesture_count = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0}  # Reset gesture count
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+cap = cv2.VideoCapture(0)
+model_path = "../1_Model_Binding/Media/5_gesture_model_8thattempt.h5"
+predict_gesture(cap, model_path)
